c6ed32499117e98308d0fe8963272c65b41525c3
Georg Eschbacher <eschbacher@gmx.at>
2014-04-11 18:57:19 +0200

    Second test: Compass should be an object

--- src/anim/main-spec.js
+++ src/anim/main-spec.js
@@ -2,4 +2,9 @@ describe("Compass -", function() {
   it("the testing framework should work properly", function() {
     expect(true).toEqual(true);
   });
+
+  it("should be an Object", function(){
+    expect(new Compass()).toEqual(jasmine.any(Object));
+  });
+
 });
--- src/anim/main.js
+++ src/anim/main.js
@@ -1,9 +1,20 @@
-var jQuery = require('jquery');
+var $ = require('jquery');
 
-var imageEl = jQuery(new Image());
-imageEl
-  .load(function() {
-    jQuery('#compassImage').append(imageEl);
-    imageEl.attr('width', '500');
-  })
-  .attr('src', '/img/compass.png');
+var imageEl = $(new Image());
+imageEl.load(function() {
+  $('#compassImage').append(imageEl);
+  imageEl.attr('width', '500');
+}).attr('src', '/img/compass.png');
+
+$(function(){
+
+});
+
+Compass = function(){
+
+}
+
+Compass.prototype = {
+
+
+}
